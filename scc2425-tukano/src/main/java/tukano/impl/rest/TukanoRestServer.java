375210b359de2534f47daa94c1a7bcaa42df135d
Eduardo Silveiro <e.silveiro@campus.fct.unl.pt>
2024-10-30 13:05:42 +0000

    200 no /tukano mas ainda nao dao os outros pedidos

--- scc2425-tukano/src/main/java/tukano/impl/rest/TukanoRestServer.java
+++ scc2425-tukano/src/main/java/tukano/impl/rest/TukanoRestServer.java
@@ -13,13 +13,14 @@ import org.glassfish.jersey.server.ResourceConfig;
 import tukano.impl.Token;
 import utils.Args;
 import utils.IP;
+import utils.Props;
 
 
 public class TukanoRestServer extends Application {
 	final private static Logger Log = Logger.getLogger(TukanoRestServer.class.getName());
 
 	static final String INETADDR_ANY = "0.0.0.0";
-	static String SERVER_BASE_URI = "http://%s:%s/rest";
+	static String SERVER_BASE_URI = "http://%s:%s/tukano/rest"; //colocar /tukano/rest
 
 	public static final int PORT = 8080;
 
@@ -37,30 +38,29 @@ public class TukanoRestServer extends Application {
 		resources.add(RestBlobsResource.class);
 		resources.add(RestUsersResource.class);
 		resources.add(RestShortsResource.class);
+		//Props.load("azurekeys-region.props"); //place the props file in resources folder under java/main
+
 
 	}
 
-//
-//	protected void start() throws Exception {
-//
-//	ResourceConfig config = new ResourceConfig();
-//		config.register(RestBlobsResource.class);
-//		config.register(RestUsersResource.class);
-//		config.register(RestShortsResource.class);
-//		config.register(new JacksonFeature());
-//		JdkHttpServerFactory.createHttpServer( URI.create(serverURI.replace(IP.hostname(), INETADDR_ANY)), config);
-//
-//		Log.info(String.format("Tukano Server ready @ %s\n",  serverURI));
-//	}
-//
-//
-//	public static void main(String[] args) throws Exception {
-//		Args.use(args);
-//
-//		Token.setSecret( Args.valueOf("-secret", ""));
-////		Props.load( Args.valueOf("-props", "").split(","));
-//
-//		new TukanoRestServer().start();
-//	}
+
+	protected void start() throws Exception {
+
+	ResourceConfig config = new ResourceConfig();
+
+		JdkHttpServerFactory.createHttpServer( URI.create(serverURI.replace(IP.hostname(), INETADDR_ANY)), config);
+
+		Log.info(String.format("Tukano Server ready @ %s\n",  serverURI));
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		Args.use(args);
+
+		Token.setSecret( Args.valueOf("-secret", ""));
+		Props.load( Args.valueOf("-props", "").split(","));
+
+		new TukanoRestServer().start();
+	}
 
 }
--- scc2425-tukano/src/main/java/utils/Props.java
+++ scc2425-tukano/src/main/java/utils/Props.java
@@ -4,6 +4,18 @@ import java.util.Arrays;
 
 public class Props {
 
+
+	public static String get(String key, String defaultValue) {
+		var val = System.getProperty(key);
+		return val == null ? defaultValue : val;
+	}
+
+	public static <T> T get(String key, Class<T> clazz) {
+		var val = System.getProperty(key);
+		if( val == null )
+			return null;
+		return JSON.decode(val, clazz);
+	}
 	public static void load(String[] keyValuePairs) {
 		System.out.println(Arrays.asList( keyValuePairs));
 		for( var pair: keyValuePairs ) {
